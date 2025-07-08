39e7281a23d2fdde0d4b71e59c74d6e36a1bc4b9
MBatuhanOzer <batuhanozer1042@gmail.com>
2025-07-08 14:58:14 +0300

    Bug fixes

--- filter_multi/filter.cpp
+++ filter_multi/filter.cpp
@@ -1,16 +1,15 @@
-
 #include "filter.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <windows.h> 
 
 // Number of chunks to divide the image into for processing
 constexpr int WORK_ITEMS = 100;
+
 // Number of threads to use for processing
-constexpr int THREAD_COUNT = 4;
-// Function declarations
-void grayscale_work(WorkItem* work);
+constexpr int THREAD_COUNT = 20;
 
 // Thread related structs and functions
 typedef struct WorkItem {
@@ -20,15 +19,16 @@ typedef struct WorkItem {
 typedef struct Thread_Work_Context {
 	WorkItem* works;
 	unsigned int work_count;
-	unsigned int work_index;
+	volatile int64_t work_index;
 } Thread_Work_Context;
-DWORD turn_grayscale_thread(void *params) {
+void grayscale_work(WorkItem* work);
+DWORD WINAPI turn_grayscale_thread(void *params) {
 	Thread_Work_Context* context = (Thread_Work_Context*)params;
 	while (true) {
-		unsigned int our_item = _InterlockedIncrement64(&context->work_index);
+		int64_t our_item = InterlockedIncrement64(&context->work_index);
 		our_item--;
 		if (our_item >= context->work_count) break;
-		Work_Item* work = &context->works[our_item];
+		WorkItem *work = &context->works[our_item];
 		grayscale_work(work);
 	}
 	return 0;
@@ -69,7 +69,7 @@ void grayscale(Image* img) {
 	Thread_Work_Context *context = create_thread_work_context(img);
 	HANDLE threads[THREAD_COUNT] = { 0 };
 	for (unsigned int i = 0; i < THREAD_COUNT; ++i) {
-		threads[i] = CreateThread(NULL, 0, turn_grayscale_thread, context, 0, NULL)
+		threads[i] = CreateThread(NULL, 0, turn_grayscale_thread, context, 0, NULL);
 	}
 	WaitForMultipleObjects(THREAD_COUNT, threads, TRUE, INFINITE);
 	destroy_thread_work_context(context);
@@ -83,7 +83,6 @@ void grayscale_work(WorkItem *work) {
 		unsigned char r = work->image[idx];
 		unsigned char g = work->image[idx + 1];
 		unsigned char b = work->image[idx + 2];
-
 		unsigned char gray = (unsigned char)(
 			0.299f * r + 0.587f * g + 0.114f * b
 		);
